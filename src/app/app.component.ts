b220e6330e1545bcf3c12d95664c6776bb34a965
Wilder Arredondo Torres <wildersoft@gmail.com>
2020-07-06 18:27:47 -0500

    changes on script for Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 't1';
+
+  ngOnInit() : void {
+
+    const numbers$ = interval(1000).pipe(
+      take(5)
+    ).subscribe(x=> console.log(x));
+
+  }
+
+  ngOnDestroy(): void {
+  }
 }
